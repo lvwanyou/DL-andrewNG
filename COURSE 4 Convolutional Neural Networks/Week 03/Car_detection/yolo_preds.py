6acde5a975d1a867ec1d1c7b4e41ad3b1fc7f93d
lvwanyou <lvwanyou@163.com>
2019-09-05 14:41:40 +0800

    Add files via upload

---
+++ COURSE 4 Convolutional Neural Networks/Week 03/Car_detection/yolo_preds.py
@@ -0,0 +1,147 @@
+import argparse
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import PIL
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Input, Lambda, Conv2D
+from keras.models import load_model, Model
+from yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
+from yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body, yolo_eval
+
+
+# GRADED FUNCTION: iou
+
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    ### START CODE HERE ### (≈ 5 lines)
+    xi1 = max(box1[0], box2[0])
+    yi1 = max(box1[1], box2[1])
+    xi2 = min(box1[2], box2[2])
+    yi2 = min(box1[3], box2[3])
+    inter_area = (xi2 - xi1) * (yi2 - yi1)
+    ### END CODE HERE ###
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    ### START CODE HERE ### (≈ 3 lines)
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    union_area = box1_area + box2_area - inter_area
+    ### END CODE HERE ###
+
+    # compute the IoU
+    ### START CODE HERE ### (≈ 1 line)
+    iou = inter_area / union_area
+    ### END CODE HERE ###
+
+    return iou
+
+
+# GRADED FUNCTION: yolo_non_max_suppression
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
+    """
+    Applies Non-max suppression (NMS) to set of boxes
+
+    Arguments:
+    scores -- tensor of shape (None,), output of yolo_filter_boxes()
+    boxes -- tensor of shape (None, 4), output of yolo_filter_boxes() that have been scaled to the image size (see later)
+    classes -- tensor of shape (None,), output of yolo_filter_boxes()
+    max_boxes -- integer, maximum number of predicted boxes you'd like
+    iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+
+    Returns:
+    scores -- tensor of shape (, None), predicted score for each box
+    boxes -- tensor of shape (4, None), predicted box coordinates
+    classes -- tensor of shape (, None), predicted class for each box
+
+    Note: The "None" dimension of the output tensors has obviously to be less than max_boxes. Note also that this
+    function will transpose the shapes of scores, boxes, classes. This is made for convenience.
+    """
+
+    max_boxes_tensor = K.variable(max_boxes, dtype='int32')  # tensor to be used in tf.image.non_max_suppression()
+    K.get_session().run(tf.variables_initializer([max_boxes_tensor]))  # initialize variable max_boxes_tensor
+
+    # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
+    ### START CODE HERE ### (≈ 1 line)
+    nms_indices = tf.image.non_max_suppression(boxes, scores, max_boxes, iou_threshold, name=None)
+    ### END CODE HERE ###
+
+    # Use K.gather() to select only nms_indices from scores, boxes and classes
+    ### START CODE HERE ### (≈ 3 lines)
+    scores = K.gather(scores, nms_indices)
+    boxes = K.gather(boxes, nms_indices)
+    classes = K.gather(classes, nms_indices)
+    ### END CODE HERE ###
+
+    return scores, boxes, classes
+
+
+def predict(sess, image_file):
+    """
+    Runs the graph stored in "sess" to predict boxes for "image_file". Prints and plots the preditions.
+
+    Arguments:
+    sess -- your tensorflow/Keras session containing the YOLO graph
+    image_file -- name of an image stored in the "images" folder.
+
+    Returns:
+    out_scores -- tensor of shape (None, ), scores of the predicted boxes
+    out_boxes -- tensor of shape (None, 4), coordinates of the predicted boxes
+    out_classes -- tensor of shape (None, ), class index of the predicted boxes
+
+    Note: "None" actually represents the number of predicted boxes, it varies between 0 and max_boxes.
+    """
+
+    # Preprocess your image
+    image, image_data = preprocess_image("images/" + image_file, model_image_size=(608, 608))
+
+    # Run the session with the correct tensors and choose the correct placeholders in the feed_dict.
+    # You'll need to use feed_dict={yolo_model.input: ... , K.learning_phase(): 0})
+    ### START CODE HERE ### (≈ 1 line)
+    out_boxes, out_scores, out_classes = sess.run([scores, boxes, classes],
+                                                  feed_dict={yolo_model.input: image_data, K.learning_phase(): 0.})
+    ### END CODE HERE ###
+
+    # Print predictions info
+    print('Found {} boxes for {}'.format(len(out_boxes), image_file))
+    # Generate colors for drawing bounding boxes.
+    colors = generate_colors(class_names)
+    # Draw bounding boxes on the image file
+    draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors)
+    # Save the predicted bounding box on the image
+    image.save(os.path.join("out", image_file), quality=90)
+    # Display the results in the notebook
+    output_image = scipy.misc.imread(os.path.join("out", image_file))
+    imshow(output_image)
+
+    return out_scores, out_boxes, out_classes
+
+
+if __name__ == "__main__":
+    sess = K.get_session()
+    class_names = read_classes("model_data/coco_classes.txt")   # 80 classes & 1 pc & 4 paras for boxes
+    anchors = read_anchors("model_data/yolo_anchors.txt")   # the parameters of anchor boxes
+    image_shape = (720., 1280.)
+    yolo_model = load_model("model_data/yolo.h5")
+    yolo_model.summary()
+
+    # Convert final layer features to bounding box parameters.
+    yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
+
+    # Evaluate YOLO model on given input batch and return filtered boxes.
+    scores, boxes, classes = yolo_eval(yolo_outputs, image_shape)
+
+    out_scores, out_boxes, out_classes = predict(sess, "test.jpg")
